be15092468a37cb6752de13ba73d68683119e10d
weiyanghe <814186463@qq.com>
2023-01-01 17:26:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="123.249.82.183";
+        url.hostname="http://123.249.82.183:5244";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
