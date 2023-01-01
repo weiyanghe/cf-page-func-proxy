0eee1122c5858b64688d45446b6f2744c634c852
weiyanghe <814186463@qq.com>
2023-01-01 17:09:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="weiyang.eu.org";
+        url.hostname="123.249.82.183";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
